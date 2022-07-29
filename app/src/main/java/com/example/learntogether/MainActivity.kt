dd4e8027ceaef26540101448f150b56fca4e08e6
alexander-laputko <alexander.laputko@ya.ru>
2022-07-29 19:53:28 +0300

    Created an article page.

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
@@ -22,7 +27,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ArticlePage(
+                        title = stringResource(id = R.string.title),
+                        text1 = stringResource(id = R.string.text_1),
+                        text2 = stringResource(id = R.string.text_2)
+                    )
                 }
             }
         }
@@ -30,14 +39,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ArticlePage(title: String, text1: String, text2: String) {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+
+    Column {
+        Image(painter = image, contentDescription = null, modifier = Modifier.fillMaxWidth())
+        Text(text = title)
+        Text(text = text1)
+        Text(text = text2)
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        ArticlePage(
+            title = stringResource(id = R.string.title),
+            text1 = stringResource(id = R.string.text_1),
+            text2 = stringResource(id = R.string.text_2)
+        )
     }
 }
