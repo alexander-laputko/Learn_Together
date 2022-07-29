1b9acd0448b77e5845c4ea232baa7ac11605a561
alexander-laputko <alexander.laputko@ya.ru>
2022-07-29 22:25:56 +0300

    The article has become more beautiful.

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -7,14 +7,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,10 +48,26 @@ fun ArticlePage(title: String, text1: String, text2: String) {
     val image = painterResource(id = R.drawable.bg_compose_background)
 
     Column {
-        Image(painter = image, contentDescription = null, modifier = Modifier.fillMaxWidth())
-        Text(text = title)
-        Text(text = text1)
-        Text(text = text2)
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+        )
+        Text(
+            text = text1,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = text2,
+            modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp),
+            textAlign = TextAlign.Justify
+        )
     }
 }
 
